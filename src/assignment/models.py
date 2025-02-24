396dd57e48bbaeacdd6226b99bcbd369ae4a326b
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-02-25 00:05:53 +0200

    Design Student Solution , assignment, grade model

--- src/assignment/models.py
+++ src/assignment/models.py
@@ -15,8 +15,10 @@ class Assignment(models.Model):
 
 class Grade(models.Model):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
-    student = models.ForeignKey(Profile, on_delete=models.CASCADE, limit_choices_to={'role': 'student'})
-    grade = models.DecimalField(max_digits=5, decimal_places=2)
+    student    = models.ForeignKey(Profile, on_delete=models.CASCADE, limit_choices_to={'role': 'student'})
+    sloution   = models.TextField()
+    grade      = models.DecimalField(max_digits=5, decimal_places=2)
 
     def _str_(self):
         return f'{self.student.name} - {self.assignment.title} - {self.grade}'
+    
